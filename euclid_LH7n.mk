f1385207bdcf5d5ae82eea9b74c713fccadf77a6
LORD | CRAZY_JOKER67 <seleznevandrey186@outlook.com>
2026-01-08 15:18:11 +0300

    LH7n: EuclidOS
    Updated product name and added euclid-specific configurations.

--- euclid_LH7n.mk
+++ euclid_LH7n.mk
@@ -10,11 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/LH7n/device.mk)
 
-# Inherit some common mica stuff.
-$(call inherit-product, vendor/mica/config/common_full_phone.mk)
+# Inherit some common euclid stuff.
+$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
 
 BOARD_VENDOR := TECNO
-PRODUCT_NAME := mica_LH7n
+PRODUCT_NAME := euclid_LH7n
 PRODUCT_DEVICE := LH7n
 PRODUCT_MANUFACTURER := TECNO
 PRODUCT_BRAND := TECNO
@@ -28,3 +28,23 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+EUCLID_MAINTAINER := CRAZY_JOKER67
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+
+# Gapps
+EUCLID_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false  
+TARGET_BUILD_BCR := true (For Basic Call Recorder)
+TARGET_BUILD_DOTGALLERY := false (For Including DotGallery)
+
+# misc
+TARGET_SUPPORTS_TOUCHGESTURES := true
+EUCLID_DEVICE := TECNO_POVA_5
+EUCLID_PROCESSOR := Mediatek_G99
+EUCLID_BUILD_TYPE := UNOFFICIAL
